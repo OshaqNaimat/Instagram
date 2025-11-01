ad8accff588248631059a956e85a8c61b436b6fe
oshaq naimat <oshaqnaimat15@gmail.com>
2025-11-01 08:34:19 +0500

    grids

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,25 +6,28 @@ import Followers from './components/Followers'
 const Home = () => {
   return (
     <>
-   <div className="grid grid-cols-4">
+      <div className="grid grid-cols-4 min-h-screen">
 
-  {/* Sidebar */}
-  <div className="hidden md:block md:col-span-1 lg:col-span-1">
-    <Sidebar />
-  </div>
+        {/* Sidebar */}
+        <div className="hidden md:block md:col-span-1 lg:col-span-1">
+          <Sidebar isMobile={false} />
+        </div>
 
-  {/* Posts */}
-  <div className="col-span-4 md:col-span-3 lg:col-span-2">
-    <Posts />
-  </div>
+        {/* Posts */}
+        <div className="col-span-4 md:col-span-3 lg:col-span-2">
+          <Posts />
+        </div>
 
-  {/* Followers */}
-  <div className="hidden lg:block lg:col-span-1">
-    <Followers />
-  </div>
+        {/* Followers */}
+        <div className="hidden lg:block lg:col-span-1">
+          <Followers />
+        </div>
 
-</div>
-j
+        {/* Bottom Navbar for small screens */}
+        <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md flex justify-around py-3 md:hidden">
+          <Sidebar isMobile={true} />
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import sidebarItems from './Data'
 
-const Sidebar = () => {
+const Sidebar = ({ isMobile }) => {
   return (
-    <div className="flex min-h-screen flex-col p-5 w-max select-none border border-r-[1px] border-gray-500">
-        <img className='w-[70px] h-[70px]' src="https://images.vexels.com/media/users/3/137198/isolated/lists/07f0d7b69ef071571e4ada2f4d6a053a-instagram-icon-colorful.png" alt="" />
-      <ul className='my-4'>
+    <div
+      className={`flex flex-col p-5 select-none 
+      ${isMobile ? 'flex-row justify-around w-full p-2' : 'min-h-screen w-max border-r border-gray-500'}`}
+    >
+      {/* Instagram Logo */}
+      {!isMobile && (
+        <img className="hidden sm:block w-[70px] h-[70px]" 
+        src="https://images.vexels.com/media/users/3/137198/isolated/lists/07f0d7b69ef071571e4ada2f4d6a053a-instagram-icon-colorful.png" alt="" />
+      )}
+
+      <ul className={`my-4 ${isMobile ? 'flex flex-row w-full justify-between' : ''}`}>
         {sidebarItems.map((item) => (
           <li
             key={item.id}
-            className="flex  items-center gap-3 py-3 px-2 my-2 text-[25px] cursor-pointer
-            rounded-md hover:bg-gray-200 duration-100 active:scale-95 "
+            className={`flex items-center gap-3 py-3 px-2 my-2 text-[25px] cursor-pointer
+            rounded-md hover:bg-gray-200 duration-100 active:scale-95 
+            ${isMobile && '!my-0'}`}
           >
             {item.icon}
-            <span className='md:block hidden  justify-center'>{item.title}</span>
+            {/* Title hidden below LG */}
+            <span className="hidden lg:block">{item.title}</span>
           </li>
         ))}
       </ul>
