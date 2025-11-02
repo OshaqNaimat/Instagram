aef7bead8da79fae0db71ebbda26573482c099a7
oshaq naimat <oshaqnaimat15@gmail.com>
2025-11-02 12:35:53 +0500

    posting

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { FaArrowLeftLong } from "react-icons/fa6";
+import { BsZoomIn } from "react-icons/bs";
 
 
 const PostModal = () => {
      const [imagePreview,setImagePreview] = useState(null)
+     const [zoomin,setZoomIn] = useState(false)
 
      const handleImageViewer = (e)=>{
         let post = e.target.files[0]
@@ -13,13 +15,24 @@ const PostModal = () => {
   return (
     <>
     <div className="bg-black/50 min-h-screen fixed top-0 w-full z-5 flex justify-center items-center">
-        <div className="xl:w-1/3 md:w-1/2 w-[45%] rounded-md   bg-white flex-col justify-center items-center ">
+        <div className="xl:w-1/3 md:w-1/2 w-[45%] rounded-3xl relative overflow-hidden  bg-white flex-col justify-center items-center ">
             { imagePreview ? 
                 (
                     <>
-                    <FaArrowLeftLong/>
+                    <div className="flex justify-between items-center w-full p-1">
+                    <FaArrowLeftLong size={20} className='cursor-pointer'/>
               <h3 className='font-semibold text-center p-2'>Create a post</h3>
-              <img src={imagePreview} className='aspect-square w-full object-fit-cover'/>      
+                      <p className='cursor-pointer text-purple-600'>Next</p>
+                    </div>
+              <img src={imagePreview} className='aspect-square  w-full object-cover'/>
+                <div onClick={()=>setZoomIn(true)} className={`bg-black/50 h-7 w-7 flex items-center justify-center 
+                absolute bottom-3 left-3 rounded-full ${zoomin ? 'bg-white text-black' : 'bg-black text-white'}`}>
+              <BsZoomIn />
+
+              <div className="bg-black/50 px-2 py-1 absolute rounded-lg translate-x-1/2 -translate-y-[100%]">
+               <input type="range"  />
+              </div>
+                </div>      
                     </>
                 )             
         : (
