a22103033b6ca544beaff5d5579689b44e6e8ef9
oshaq naimat <oshaqnaimat15@gmail.com>
2025-11-16 03:06:18 +0500

    stories

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Speoples from "./Speoples";
 
 const Stories = () => {
-  return <></>;
+  return (
+    <>
+      <div className="flex gap-3 overflow-x-scroll w-100">
+        <Speoples />
+        <Speoples />
+        <Speoples />
+        <Speoples />
+        <Speoples />
+        <Speoples />
+        <Speoples />
+        <Speoples />
+        <Speoples />
+        <Speoples />
+      </div>
+    </>
+  );
 };
 
 export default Stories;
