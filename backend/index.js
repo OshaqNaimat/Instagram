96b1ee5b0434ec9fa28b84e643dccff05265f430
oshaq naimat <oshaqnaimat15@gmail.com>
2025-11-28 21:49:50 +0500

    middleware issue

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import color from "colors";
 import { connectDB } from "./config/connect.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(errorHandler);
 connectDB();
 
 app.use("/api/posts", postRouter);
